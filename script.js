bba41d9f55150bb97118440e22674b06ce52e6fd
NilayD-lab <71390492+NilayD-lab@users.noreply.github.com>
2021-07-21 19:36:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let array= []
 link.onclick=function(){
     array = input.value.split("")
     //sessionStorage.setItem('array', JSON.stringify(array))
-    localStorage.setItem("vOneLocalStorage", array)
-    window.location.href = 'open.html'
+    //localStorage.setItem("vOneLocalStorage", array)
+    window.document.location = './open.html'+'?arr=' + input.value
+    //window.location.href = 'open.html'
 }
