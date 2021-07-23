419eca14742128f8211959d20268cd3abe6648d8
NilayD-lab <71390492+NilayD-lab@users.noreply.github.com>
2021-07-23 08:07:27 -0400

    Update test.js

--- test.js
+++ test.js
@@ -12,21 +12,46 @@ let timer;
 let alphabetDone= false;
 let cycleDone = false;
 let savedInput = [];
-//savedInput = JSON.parse(sessionStorage.getItem('array'))
-//savedInput = localStorage.getItem("vOneLocalStorage").split(",")
-savedInput = document.location.search.replace(/^.*?\=/, "").split("");
+for (i=0;i<153;i++){
+    savedInput[i] = " "
+}
+let thing = detranslate(document.location.search.replace(/^.*?\=/, ""));
+for (i=0;i<thing.length;i++){
+    savedInput[i] = thing[i]
+}
 console.log(savedInput)
 let cycleEnded = [];
 let cycle = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!#&?;. ".split("");
 let countDown;
 
 
-
+function detranslate(message){
+    let num = "";
+    let decodedMessage = ""
+    let shift = parseInt(message[0] + message[2])
+    for (i=4;i<message.length;i++){
+        if (message[i]=="%"){
+            for (z=i+1;message[z]!="%";z++){
+                num+=message[z];
+            }
+            for (j=0;j<parseInt(num);j++){
+                decodedMessage += " "
+                console.log("ddd")
+            }
+            i+=num.length+1
+            console.log(num.length)
+        }
+        else{
+            decodedMessage += String.fromCharCode(message[i].charCodeAt(0)-shift)
+        }
+    }
+    return decodedMessage.split("")
+}
 
 for (r=0;r<9;r++){
     table.innerHTML+="<tr id=\"row" + r + "\"></tr>"
     for (c=0;c<17;c++){
-        document.getElementById("row"+r).innerHTML += "<td><input type=\"text\" id="+count+" maxlength=\"1\" readonly=\"true\" /></td>"
+        document.getElementById("row"+r).innerHTML += "<td><input type=\"text\" id="+count+" readonly=\"true\" /></td>"
         count++;
     }
 }
@@ -131,4 +156,3 @@ function over(){
     button.classList.remove('go-back')
     button.classList.add('touching')
 }
-
